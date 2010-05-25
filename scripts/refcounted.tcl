81175883f41fbf7b0b9b757372be462d9ce287ee
Cyan Ogilvie <cyan@tirion.(none)>
2010-05-25 09:02:45 +0200

    Allowed refcount to use a dictionary entry as an object registry

--- scripts/refcounted.tcl
+++ scripts/refcounted.tcl
@@ -2,6 +2,7 @@
 
 oo::class create cflib::refcounted {
 	constructor {} {
+		set keychain	{}
 		if {[self next] ne {}} {next}
 		set refcount	1
 	}
@@ -14,31 +15,37 @@ oo::class create cflib::refcounted {
 	variable {*}{
 		refcount
 		registry_var
+		keychain
 	}
 
-	method object_registry {varname} { #<<<
+	method object_registry {varname args} { #<<<
 		my _clear_registry
 
 		set registry_var	$varname
 		upvar $registry_var var_ref
 
-		set var_ref		[self]
+		set keychain		$args
+		if {[llength $keychain] == 0} {
+			set var_ref		[self]
+		} else {
+			dict set var_ref {*}$keychain [self]
+		}
 	}
 
 	#>>>
 	method incref {args} { #<<<
-		set old		$refcount
+		#set old		$refcount
 		incr refcount
-		my log_cmd debug "[self]: refcount $old -> $refcount ($args)"
+		#my log_cmd debug "[self]: refcount $old -> $refcount ($args)"
 	}
 
 	#>>>
 	method decref {args} { #<<<
-		set old		$refcount
+		#set old		$refcount
 		incr refcount -1
-		my log_cmd debug "[self]: refcount $old -> $refcount ($args)"
+		#my log_cmd debug "[self]: refcount $old -> $refcount ($args)"
 		if {$refcount <= 0} {
-			my log_cmd debug "[self]: our time has come"
+			#my log_cmd debug "[self]: our time has come"
 			my destroy
 			return
 		}
@@ -46,17 +53,17 @@ oo::class create cflib::refcounted {
 
 	#>>>
 	method refcount {} { #<<<
-		return $refcount
+		set refcount
 	}
 
 	#>>>
 
 	method log_cmd {lvl msg args} {}
 	method autoscoperef {} { #<<<
-		my log_cmd debug "[self class]::[self method] callstack: (callstack dump broken)"
+		#my log_cmd debug "[self class]::[self method] callstack: (callstack dump broken)"
 		upvar 2 _cflib_refcounted_scoperef_[string map {:: //} [self]] scopevar
 		set scopevar	[self]
-		trace variable scopevar u [namespace code {my decref "scopevar unset"}]
+		trace add variable scopevar {unset} [namespace code {my decref "scopevar unset"}]
 	}
 
 	#>>>
@@ -64,7 +71,15 @@ oo::class create cflib::refcounted {
 		if {[info exists registry_var]} {
 			upvar $registry_var old_registry
 			if {[info exists old_registry]} {
-				unset old_registry
+				if {[llength $keychain] == 0} {
+					unset old_registry
+				} else {
+					try {
+						dict unset old_registry {*}$keychain
+					} on error {errmsg options} {
+						puts stderr "Could not remove object registry entry: $errmsg"
+					}
+				}
 			}
 		}
 	}
