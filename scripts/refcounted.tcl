f0ce8e427b4efe5b3ffc1af2f87493d59a5119f6
Cyan Ogilvie <cyan@tirion.(none)>
2011-05-23 18:50:33 +0200

    Enhanced cflib::refcounted to support bound name references and manual scopevar references

--- scripts/refcounted.tcl
+++ scripts/refcounted.tcl
@@ -1,21 +1,21 @@
 # vim: ft=tcl foldmethod=marker foldmarker=<<<,>>> ts=4 shiftwidth=4
 
 oo::class create cflib::refcounted {
+	variable {*}{
+		refcount
+		registry_var
+		keychain
+	}
+
 	constructor {} {
 		set keychain	{}
-		if {[self next] ne {}} {next}
+		if {[self next] ne ""} next
 		set refcount	1
 	}
 
 	destructor {
 		my _clear_registry
-		if {[self next] ne {}} {next}
-	}
-
-	variable {*}{
-		refcount
-		registry_var
-		keychain
+		if {[self next] ne ""} next
 	}
 
 	method object_registry {varname args} { #<<<
@@ -54,6 +54,24 @@ oo::class create cflib::refcounted {
 	#>>>
 	method refcount {} { #<<<
 		set refcount
+	}
+
+	#>>>
+	method bind_name {name} { #<<<
+		set ns	[uplevel 2 {namespace current}]
+		if {$ns eq "::"} {set ns	""}
+		set fqname	${ns}::$name
+		interp alias {} $fqname {} [self]
+		my incref "bound_name $name"
+		trace add command $fqname delete [namespace code [list my decref "bound_name $name"]]
+	}
+
+	#>>>
+	method additional_scoperef {desc} { #<<<
+		upvar 2 _cflib_refcounted_scoperef_[string map {:: //} [self]]_[incr ::cflib::scoperef] scopevar
+		set scopevar	[self]
+		my incref "manual scopevar $desc"
+		trace add variable scopevar unset [namespace code [list my decref "manual scopevar $desc unset"]]
 	}
 
 	#>>>
