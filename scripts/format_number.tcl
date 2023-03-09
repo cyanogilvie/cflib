ea743e19a4c56272ba27950f2670c698377f29da
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2023-03-09 20:42:24 +0200

    Performance fix for format_number

--- scripts/format_number.tcl
+++ scripts/format_number.tcl
@@ -1,6 +1,7 @@
 proc cflib::format_number {n {sep ,}} {
-	if {![regexp {^([0-9]{0,3})((?:[0-9]{3})*)(\.[0-9]+)?$} $n - l r d]} {
-		error "Not an integer: $n"
+	if {![regexp {^([0-9]*)(\.[0-9]+)?$} $n - a d]} {
+		error "Not an number: $n"
 	}
+	regexp {^([0-9]{0,3})((?:[0-9]{3})*)$} $a - l r
 	string cat [join [list {*}$l {*}[regexp -all -inline ... $r]] $sep] $d
 }
