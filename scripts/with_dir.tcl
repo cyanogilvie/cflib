dea75ed6b1dce6fe274917f31401acc400bb0336
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2024-09-12 11:06:09 +0200

    Fix syntax

--- scripts/with_dir.tcl
+++ scripts/with_dir.tcl
@@ -4,7 +4,7 @@ proc cflib::with_dir {dir script} {
 	set hold	[pwd]
 	set code	[catch {uplevel 1 $script} r o]
 	cd $hold
-	if {$code == 1} {return -code $code $r $o}	;# Propagate error stack unwind
+	if {$code == 1} {return -code $code -options $o $r}	;# Propagate error stack unwind
 	return -code $code $r
 }
 
