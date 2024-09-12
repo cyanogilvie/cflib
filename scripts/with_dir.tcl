ea9086d0f3a167921f0455f89c600a0cd5903afc
Cyan Ogilvie <cyan.ogilvie@gmail.com>
2024-09-12 11:01:07 +0200

    Transparent control structures like this are slightly more efficiently implemented with catch

--- scripts/with_dir.tcl
+++ scripts/with_dir.tcl
@@ -2,17 +2,8 @@
 
 proc cflib::with_dir {dir script} {
 	set hold	[pwd]
-	try {
-		uplevel 1 $script
-	} on break {r o} - on continue {r o} {
-		dict incr o -level 1
-		return -options $o $r
-	} on return {r o} {
-		dict incr o -level 1
-		dict set o -code return
-		return -options $o $r
-	} finally {
-		cd $hold
-	}
+	set code	[catch {uplevel 1 $script} r]
+	cd $hold
+	return -code $code $r
 }
 
